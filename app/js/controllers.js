d8982f8aee98897460f38cb7866638c9794ceba3
Vitaliy <elanceShevchenko@gmail.com>
2015-02-26 02:40:22 +0200

    added latest ui-grid build

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,27 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', [function() {
-
+  .controller('MyCtrl1', ['$scope',function($scope) {
+      $scope.myData = [
+        {
+          "firstName": "Cox",
+          "lastName": "Carney",
+          "company": "Enormo",
+          "employed": true
+        },
+        {
+          "firstName": "Lorraine",
+          "lastName": "Wise",
+          "company": "Comveyer",
+          "employed": false
+        },
+        {
+          "firstName": "Nancy",
+          "lastName": "Waters",
+          "company": "Fuelton",
+          "employed": false
+        }
+      ];
   }])
   .controller('MyCtrl2', [function() {
 
